b1adda947a7cba06a630dc850f2bbaff32564ff2
denivladislav <deni.vladislav@gmail.com>
2024-10-05 21:57:54 +0300

    feat: loading spinner is separated; moved to shared module

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -8,7 +8,6 @@ import {
     FormGroup,
     FormGroupDirective,
 } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -30,7 +29,6 @@ import { SharedModule } from '../../modules';
         MatFormFieldModule,
         MatButtonModule,
         MatInputModule,
-        MatProgressSpinnerModule,
         ToDoListItemComponent,
         ToDoListItemDescriptionComponent,
         ButtonComponent,
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CustomTooltipDirective } from '../../directives/custom-tooltip.directive';
+import { LoadingSpinnerComponent } from '../../components/loading-spinner/loading-spinner.component';
 
 @NgModule({
+    imports: [LoadingSpinnerComponent],
     declarations: [CustomTooltipDirective],
-    exports: [CustomTooltipDirective],
+    exports: [CustomTooltipDirective, LoadingSpinnerComponent],
 })
 export class SharedModule {}
