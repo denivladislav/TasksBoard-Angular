d055cfbbe8edbb454ba3d57067dd8b26f23183df
denivladislav <deni.vladislav@gmail.com>
2024-11-27 11:36:18 +0300

    fix: removed redundant import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ToDoListComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
